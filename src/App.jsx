33008466e687175964d3bca61a639a061295f358
beeplovsharma <beeplovsharma@gmail.com>
2024-01-27 12:12:44 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Basics from './components/Basics'
-import Cycle from './components/Cycle'
-import Drag from './components/Drag'
-import ExpandCard from './components/ExpandCard'
-import Sequence from './components/Sequence'
-import DeskFirst from './components/DeskFirst'
-import { MaskText } from './components/MaskText'
+import { useState } from "react";
+import "./App.css";
+import Basics from "./components/Basics";
+import Cycle from "./components/Cycle";
+import Drag from "./components/Drag";
+import ExpandCard from "./components/ExpandCard";
+import Sequence from "./components/Sequence";
+import DeskFirst from "./components/DeskFirst";
+import { MaskText } from "./components/MaskText";
 function App() {
   return (
     <>
-      <div className='w-full min-h-screen'>
-        <ExpandCard/>
-        <Basics/>
-        <Cycle/>
-        <Drag/>
-        <Sequence/>
-        <DeskFirst/>
-        <MaskText/>
+      <div className="w-full min-h-screen">
+        <ExpandCard />
+        <Basics />
+        <Cycle />
+        <Drag />
+        <Sequence />
+        <DeskFirst />
+        {/* <MaskText/> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
